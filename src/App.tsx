703c48038304292fbf1417e70d511d6405f78a91
Nickolay Dudaryk <nick.dudaryk@gmail.com>
2021-03-24 20:11:20 +0300

    feat: header & app routing

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,26 @@ import MiniGames from './pages/MiniGames/MiniGames';
 import Settings from './pages/Settings/Settings';
 import Statistics from './pages/Statistics/Statistics';
 import TextBook from './pages/TextBook/TextBook';
+import AboutTeam from './pages/AboutTeam/AboutTeam';
+// Components
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path={'/main'} component={Main} exact />
-        <Route path={'/mini-games'} component={MiniGames} exact />
-        <Route path={'/settings'} component={Settings} exact />
-        <Route path={'/statistics'} component={Statistics} exact />
-        <Route path={'/text-book'} component={TextBook} exact />
-        <Redirect to={'/main'} />
-      </Switch>
-    </Router>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path={'/main'} component={Main} exact />
+          <Route path={'/mini-games'} component={MiniGames} exact />
+          <Route path={'/settings'} component={Settings} exact />
+          <Route path={'/statistics'} component={Statistics} exact />
+          <Route path={'/text-book'} component={TextBook} exact />
+          <Route path={'/about-team'} component={AboutTeam} exact />
+          <Redirect to={'/main'} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
