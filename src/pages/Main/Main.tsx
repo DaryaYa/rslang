5a20d9b095fbe4d21a75fd6476d3a69afafdeae5
Nickolay Dudaryk <nick.dudaryk@gmail.com>
2021-03-24 18:28:41 +0300

    refactor: basic project layout

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.scss';
+
+const Main = () => {
+  return (
+    <div>
+      Main page
+    </div>
+  )
+}
+
+export default Main;
---
+++ src/pages/Statistics/Statistics.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Statistics.scss';
+
+const Statistics = () => {
+  return (
+    <div>
+      Statistics page
+    </div>
+  )
+}
+
+export default Statistics
