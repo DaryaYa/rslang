81e294680726089436e1e8f740ae0f2d971b9849
DaryaYa <lyalyaka@mail.ru>
2021-04-04 18:42:09 +0300

    rebase

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import {
   Redirect,
 } from "react-router-dom";
 // PAGES
+<<<<<<< HEAD
 import Main from "./pages/Main/Main";
 import MiniGames from "./pages/MiniGames/MiniGames";
 import Settings from "./pages/Settings/Settings";
@@ -18,6 +19,15 @@ import { Statistics } from "./pages/Statistics/Statistics";
 import TextBook from "./pages/TextBook/TextBook";
 import AboutTeam from "./pages/AboutTeam/AboutTeam";
 import AudioCall from "./pages/MiniGames/AudioCall/AudioCall";
+=======
+import Main from './pages/Main/Main';
+import MiniGames from './pages/MiniGames/MiniGames';
+import Settings from './pages/Settings/Settings';
+import { Statistics } from './pages/Statistics/Statistics';
+import TextBook from './pages/TextBook/TextBook';
+import AboutTeam from './pages/AboutTeam/AboutTeam';
+import Savannah from './pages/Savannah/Savannah';
+>>>>>>> 9d85389... init
 // Components
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -29,6 +39,7 @@ const App = () => {
         <Header />
         <ToastContainer />
         <Switch>
+<<<<<<< HEAD
           <Route path={"/"} component={Main} exact />
           <Route path={"/mini-games"} component={MiniGames} exact />
           <Route path={"/audiocall"} component={AudioCall} exact />
@@ -37,6 +48,16 @@ const App = () => {
           <Route path={"/text-book"} component={TextBook} exact />
           <Route path={"/about-team"} component={AboutTeam} exact />
           <Redirect to={"/"} />
+=======
+          <Route path={'/'} component={Main} exact />
+          <Route path={'/mini-games'} component={MiniGames} exact />
+          <Route path={'/settings'} component={Settings} exact />
+          <Route path={'/statistics'} component={Statistics} exact />
+          <Route path={'/text-book'} component={TextBook} exact />
+          <Route path={'/about-team'} component={AboutTeam} exact />
+          <Route path={'/savannah'} component={Savannah} exact />
+          <Redirect to={'/'} />
+>>>>>>> 9d85389... init
         </Switch>
 
         <Footer />
